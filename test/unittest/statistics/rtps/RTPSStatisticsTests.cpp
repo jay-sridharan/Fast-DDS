fcffeb73ddf80a0ab7cf9a22856f9932a1356b90
Miguel Company <MiguelCompany@eprosima.com>
2021-04-29 15:11:05 +0200

    Refs 10789. Linters.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/unittest/statistics/rtps/RTPSStatisticsTests.cpp
+++ test/unittest/statistics/rtps/RTPSStatisticsTests.cpp
@@ -505,7 +505,7 @@ TEST_F(RTPSStatisticsTests, statistics_rpts_listener_callbacks)
     // reader callbacks through participant listener
     auto participant_reader_listener = make_shared<MockListener>();
     ASSERT_TRUE(participant_->add_statistics_listener(participant_reader_listener,
-        EventKind::ACKNACK_COUNT | EventKind::HISTORY2HISTORY_LATENCY));
+            EventKind::ACKNACK_COUNT | EventKind::HISTORY2HISTORY_LATENCY));
 
     // reader specific callbacks
     auto reader_listener = make_shared<MockListener>();
@@ -563,7 +563,7 @@ TEST_F(RTPSStatisticsTests, statistics_rpts_listener_callbacks)
     EXPECT_TRUE(participant_->remove_statistics_listener(participant_listener, EventKind::RTPS_SENT));
     EXPECT_TRUE(participant_->remove_statistics_listener(participant_writer_listener, EventKind::DATA_COUNT));
     EXPECT_TRUE(participant_->remove_statistics_listener(participant_reader_listener,
-        EventKind::ACKNACK_COUNT | EventKind::HISTORY2HISTORY_LATENCY));
+            EventKind::ACKNACK_COUNT | EventKind::HISTORY2HISTORY_LATENCY));
 }
 
 /*
